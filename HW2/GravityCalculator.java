8d9d62b4e6abc4dc585cdba9e4b5b72afc5e188e
Erik <ebobinski0@gmail.com>
2024-09-18 22:48:22 -0400

    HW2

---
+++ HW2/GravityCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GravityCalculator {
+  public static void main(String[] arguments) {
+  double gravity = -9.81; // Earth's gravity in m/s^2
+  double initialVelocity = 0.0;
+  double fallingTime = 10.0;
+  double initialPosition = 0.0;
+  double finalPosition = 0.0;
+  System.out.println("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
+  }
+}
+// the program's output is a simple print command that displays the object's fallingTime (10 seconds) and 
+// its finalPosition (0m), which makes sence because the object has the same initial and final position  
---
+++ HW2/MyDateClass.java
@@ -0,0 +1,32 @@
+public class MyDateClass {
+    
+    private int month;
+    private int day;
+    private int year;
+
+    public void setDate(int assignedMonth, int assignedDay, int assignedYear) {
+        this.month = assignedMonth;
+        this.day = assignedDay;
+        this.year = assignedYear;
+    }
+
+    public String getDate() {
+        return String.format("%d/%d/%02d", month, day, year);
+    }
+
+
+
+    public static void main(String[] args) {
+        //instantiate object
+        MyDateClass obj = new MyDateClass();
+
+        //set date values
+        obj.setDate(12,28,02);
+
+        //display date
+        System.out.println(obj.getDate());
+
+        //display name
+        System.out.println("Erik Bobinski");
+    }
+}
